2f0b320938e2e453af847fcbf5e9fe3c4b861962
goku31s <168161576+goku31s@users.noreply.github.com>
2025-08-19 18:10:17 +0530

    Create locals.tf
    Create locals.tf

---
+++ locals.tf
@@ -0,0 +1,21 @@
+locals {
+  web_tag = "web"
+
+  names = {
+    network        = "${var.name_prefix}-vpc"
+    subnet         = "${var.name_prefix}-subnet"
+    router         = "${var.name_prefix}-router"
+    nat            = "${var.name_prefix}-nat"
+    fw_http_https  = "${var.name_prefix}-allow-http-https"
+    fw_iap_ssh     = "${var.name_prefix}-allow-iap-ssh"
+    fw_hc          = "${var.name_prefix}-allow-hc"
+    instance_template = "${var.name_prefix}-instance-template"
+    mig            = "${var.name_prefix}-mig"
+    asg            = "${var.name_prefix}-autoscaler"
+    hc             = "${var.name_prefix}-hc"
+    backend        = "${var.name_prefix}-backend"
+    urlmap         = "${var.name_prefix}-urlmap"
+    proxy          = "${var.name_prefix}-proxy"
+    forwarding_rule = "${var.name_prefix}-forwarding-rule"
+  }
+}
