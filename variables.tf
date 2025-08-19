2884fc10a491bf35c1da630aec7f085dcbd1d2ba
goku31s <168161576+goku31s@users.noreply.github.com>
2025-08-19 18:01:53 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "Region for regional resources"
+  type        = string
+}
+
+variable "zone" {
+  description = "Zone for zonal resources (MIG)"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Machine type for instances"
+  type        = string
+}
+
+variable "subnet_cidr" {
+  description = "CIDR range for the subnet"
+  type        = string
+}
+
+variable "name_prefix" {
+  description = "Prefix to use for resource names"
+  type        = string
+}
+
+variable "autoscaler_min_replicas" {
+  description = "Minimum size of the MIG"
+  type        = number
+}
+
+variable "autoscaler_max_replicas" {
+  description = "Maximum size of the MIG"
+  type        = number
+}
