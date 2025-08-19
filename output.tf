a9eb86272e93df78453ccbb7b46994f0bf3a5615
goku31s <168161576+goku31s@users.noreply.github.com>
2025-08-19 18:20:18 +0530

    Create output.tf
    Create output.tf

---
+++ output.tf
@@ -0,0 +1,9 @@
+output "load_balancer_ip" {
+  description = "Global external IP of the HTTP LB"
+  value       = google_compute_global_forwarding_rule.forwarding_rule.ip_address
+}
+
+output "http_url" {
+  description = "Convenience URL for testing"
+  value       = "http://${google_compute_global_forwarding_rule.forwarding_rule.ip_address}"
+}
